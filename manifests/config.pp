c1aac868307c59d89978cc161f82ce6684155638
Antoine Rouyer <antoine.rouyer@numergy.com>
2014-03-06 13:03:06 +0100

    Fixed environment
    Use HOME and USER envvars as default for $home and $user.
    Set cwd to $home

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,12 +2,14 @@ define git::config(
   $value,
   $section  = regsubst($name, '^([^\.]+)\.([^\.]+)$','\1'),
   $key      = regsubst($name, '^([^\.]+)\.([^\.]+)$','\2'),
-  $user     = 'root',
+  $user     = inline_template('<%= ENV["USER"] %>'),
+  $home     = inline_template('<%= ENV["HOME"] %>'),
 ) {
   exec{"git config --global ${section}.${key} '${value}'":
-    environment => inline_template('<%= "HOME=" + ENV["HOME"] %>'),
+    environment => "HOME=${home}",
     path        => ['/usr/bin', '/bin'],
     unless      => "git config --global --get ${section}.${key} '${value}'",
     user        => $user,
+    cwd         => $home,
   }
 }
