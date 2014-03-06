39abe78ec3becd66f0b5f1afd1aa886e234502b0
Antoine Rouyer <antoine.rouyer@numergy.com>
2014-03-06 11:40:25 +0100

    Added user parameter for git::config
    It may be usefull to define git configuration for an other user than
    root.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,10 +2,12 @@ define git::config(
   $value,
   $section  = regsubst($name, '^([^\.]+)\.([^\.]+)$','\1'),
   $key      = regsubst($name, '^([^\.]+)\.([^\.]+)$','\2'),
+  $user     = 'root',
 ) {
   exec{"git config --global ${section}.${key} '${value}'":
     environment => inline_template('<%= "HOME=" + ENV["HOME"] %>'),
     path        => ['/usr/bin', '/bin'],
     unless      => "git config --global --get ${section}.${key} '${value}'",
+    user        => $user,
   }
 }
